54ca83b644f68ee6095562acdb230ff04a5aae81
Austin L. Howard <austin.lane.howard@gmail.com>
2025-02-11 19:55:18 -0700

    Finished initial timer build

---
+++ wwwroot/audio.js
@@ -0,0 +1,15 @@
+﻿window.playBeep = function (frequency = 440, duration = 500) {
+    const context = new (window.AudioContext || window.webkitAudioContext)();
+    const oscillator = context.createOscillator();
+    const gainNode = context.createGain();
+
+    oscillator.type = "sine"; // Can be "square", "sawtooth", "triangle"
+    oscillator.frequency.setValueAtTime(frequency, context.currentTime);
+    oscillator.connect(gainNode);
+    gainNode.connect(context.destination);
+
+    oscillator.start();
+    setTimeout(() => {
+        oscillator.stop();
+    }, duration);
+};
